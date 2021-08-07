7f490584554e62024e6da5af2c75863bbe07d089
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-08-07 19:48:47 -0400

    SEO

--- component/SEO.tsx
+++ component/SEO.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export default function SEO({ title, description }: Props) {
-    const metaDescription = description || "Free high quality unreleased music with a very familar design meant to not be different from what you normally see."
+    const metaDescription = description || "Free high quality unreleased music with a very familar design."
     title = `CatJam - ${title}`
 
   return (
